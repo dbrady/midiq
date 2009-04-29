f17f929d10274ffe103a4ed19f04c98b249c6e6c
David Brady <dbrady@shinybit.com>
2009-04-29 12:13:59 -0600

    Added num selector and srand to work_queue_server

--- work_queue_server.rb
+++ work_queue_server.rb
@@ -4,14 +4,20 @@ require 'rubygems'
 require 'beanstalk-client'
 require 'midiator'
 
+srand(Time.now.to_i)
+
 host = ARGV[0] || 'localhost'
+num = (ARGV[1] || 60).to_i
 
 bs = Beanstalk::Pool.new ["#{host}:11300"]
 
 chord = [0, 2, 5]
 
-10.times do
+puts "Sending #{num} jobs..."
+num.times do
   n = (rand(5)+4)*12 + chord[rand(3)]
   dur = 0.15 + rand() / 6
-  bs.put "S #{n} #{dur} 1 127"
+  msg = "S #{n} #{'%5.3f' % dur} 1 127"
+  puts msg
+  bs.put msg
 end
